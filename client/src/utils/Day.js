7c3b7ac775527bba549c26e4c3c7357dbcad5726
blinkz8 <blinkz8@protonmail.com>
2022-06-21 18:24:01 +0200

    Add day

---
+++ client/src/utils/Day.js
@@ -0,0 +1,20 @@
+
+class Day {
+    static get Name() {
+        return {
+            mon: "Poniedziałek",
+            tue: "Wtorek",
+            wed: "Środa",
+            thu: "Czwartek",
+            fri: "Piątek",
+            sat: "Sobota",
+            sun: "Niedziela"
+        };
+    }
+
+    constructor(day) {
+
+    }
+}
+
+module.exports = Day;
--- server/src/utils/WorkingDays.js
+++
@@ -1,4 +0,0 @@
-
-class WorkingDays {}
-
-module.exports = WorkingDays;
