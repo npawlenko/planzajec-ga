a427edf3b4d4e514ef3339a20bcc70ec11e07572
blinkz8 <blinkz8@protonmail.com>
2022-06-21 18:24:38 +0200

    Update algorithm

--- server/src/PlanZajecGA.js
+++ server/src/PlanZajecGA.js
@@ -1,100 +1,116 @@
 /**
- * Phenotype consist
+ *
  */
+
 const GA = require("geneticalgorithm");
-const phenotypeSize = 24;
-/**
- * Phenotype consists of:
- * subject, group, teacher, room, day, time
- */
+
+function randomBetween(min, max) {
+    return Math.floor(Math.random() * (max-min+1)) + min;
+}
+
+function pickRandom(arr) {
+    const index = randomBetween(0, arr.length-1);
+    return arr[index];
+}
+
 class PlanZajecGA {
     constructor(data) {
+        this.data = data;
+
         let config = {
             mutationFunction: this.mutation,
             crossoverFunction: this.crossover,
             fitnessFunction: this.fitness,
             doesABeatBFunction: this.competition,
-            population: [ this.generatePhenotype(data) ],
-            populationSize: phenotypeSize * 10
+            population: this.generatePopulation(50),
+            populationSize: 200
         }
 
         this.ga = GA(config);
     }
 
 
-    generatePhenotype(data) {
-
-        //to nie jest fenotyp tylko wszystkie dane otrzymane !!
-        const days = [
-            "mon",
-            "tue",
-            "wed",
-            "thi",
-            "fri",
-            "sat",
-            "sun"
-        ];
-
-        const dayStart = days.indexOf(data["dayStart"]);
-        const dayEnd = days.indexOf(data["dayStart"]);
-        let workingDays;
-
-        if(dayStart > dayEnd) {
-            workingDays = days.slice(dayEnd, days.length-1); // dayEnd-Sun
-            workingDays = workingDays.concat(days.slice(0, dayStart)); // Mon-dayStart
-        }
-        else
-            workingDays = days.splice(dayStart, dayEnd); // dayStart-dayEnd
-
-        data = {
-            groups: data["groups"],
-            subjects: data["subjects"],
-            teachers: data["teachers"],
-            day: workingDays,
-            time: data["time"]
+    generatePhenotype() {
+        let phenotype = {
+            day: pickRandom(this.data.days),
+            time: pickRandom(this.data.times),
+            subject: pickRandom(this.data.subjects),
+            group: pickRandom(this.data.groups),
+            teacher: pickRandom(this.data.teachers)
         };
 
-        return data;
+        return phenotype;
+    }
+
+    generatePopulation(size) {
+        let population = [];
+
+        for(let i=0; i<size; i++) {
+            population.push(this.generatePhenotype());
+        }
+
+        return population;
     }
 
 
     // Genethic algorithm functions
-
     mutation(oldPhenotype) {
         let resultPhenotype = {};
         // use oldPhenotype and some random
         // function to make a change to your
         // phenotype
 
-        // jakas mutacja
+        resultPhenotype = {
+            day: pickRandom(this.data.days),
+            time: pickRandom(this.data.times),
+            teacher: pickRandom(this.data.teacher),
+            subject: oldPhenotype.subject,
+            group: oldPhenotype.group
+        }
 
         return resultPhenotype;
     }
 
     crossover(phenotypeA, phenotypeB) {
         let result1 = {}, result2 = {};
+        // use phenotypeA and B to create phenotype result 1 and 2
 
-        // zmieniamy przedmioty np
 
-        // use phenoTypeA and B to create phenotype result 1 and 2
+        result1 = {
+            day: phenotypeA.day,
+            time: phenotypeB.time,
+            teacher: phenotypeA.teacher,
+            subject: phenotypeA.subject,
+            group: phenotypeA.group
+        };
 
+        result2 = {
+            day: phenotypeB.day,
+            time: phenotypeA.time,
+            teacher: phenotypeB.teacher,
+            subject: phenotypeB.subject,
+            group: phenotypeB.group
+        };
 
         return [result1, result2];
     }
 
     fitness(phenotype) {
         let fitness = 0;
-
-        // liczba luk w planie, moze kolizji
-
         // use phenotype and possibly some other information
         // to determine the fitness number.  Higher is better, lower is worse.
+
+        // czy przed i po elemencie nastepuja zajecia (+)
+
+
+        // czy wystepuja kolizje (-)
 
 
         return fitness;
     }
 
     competition(phenotypeA, phenotypeB) {
+        // im wyzszy fitness tym bardziej przystosowany
         return this.fitness(phenotypeA) >= this.fitness(phenotypeB)
     }
 }
